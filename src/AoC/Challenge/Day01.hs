d246ffb5912f60b50981e2e05628ad048f9cbc0b
Matthew Green <mgreen89@gmail.com>
2023-12-02 11:52:13 +0000

    Day 1a

--- src/AoC/Challenge/Day01.hs
+++ src/AoC/Challenge/Day01.hs
@@ -4,16 +4,33 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day01 (
+  day01a
   )
 where
 
--- day01a
 -- , day01b
 
 import AoC.Solution
+import Data.Char (isDigit, digitToInt)
+import Data.Maybe (mapMaybe)
+import Safe (headMay, lastMay)
+import AoC.Util (maybeToEither)
 
-day01a :: Solution _ _
-day01a = Solution{sParse = Right, sShow = show, sSolve = Right}
+getVal :: [Int] -> Maybe Int
+getVal xs =
+  (\x y -> x * 10 + y) <$> headMay xs <*> lastMay xs
+
+digitToIntMay :: Char -> Maybe Int
+digitToIntMay c
+  | isDigit c = Just $ digitToInt c
+  | otherwise = Nothing
+
+partA :: [String] -> Maybe Int
+partA =
+  fmap sum . traverse (getVal . mapMaybe digitToIntMay)
+
+day01a :: Solution [String] Int
+day01a = Solution{sParse = Right . lines, sShow = show, sSolve = maybeToEither "Invalid line" . partA}
 
 day01b :: Solution _ _
 day01b = Solution{sParse = Right, sShow = show, sSolve = Right}
