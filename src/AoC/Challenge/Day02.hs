3a94e0b04018d7e0fbaed8d717222b5911a8f00c
Matthew Green <mgreen89@gmail.com>
2023-12-02 11:27:53 +0000

    Day 2b

--- src/AoC/Challenge/Day02.hs
+++ src/AoC/Challenge/Day02.hs
@@ -1,14 +1,8 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day02
   ( day02a,
+    day02b
   )
 where
-
--- , day02b
 
 import AoC.Solution
 import Data.Bifunctor (first)
@@ -18,6 +12,7 @@ import Linear (V3 (..))
 import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
 import qualified Text.Megaparsec.Char.Lexer as MPL
+import Control.Applicative (liftA2)
 
 type Pull = V3 Int
 
@@ -65,8 +60,12 @@ partA :: [Game] -> Int
 partA =
   sum . map fst . filter (\(_, ps) -> all isPossible ps)
 
-day02a :: Solution _ _
+day02a :: Solution [Game] Int
 day02a = Solution {sParse = parseInput, sShow = show, sSolve = Right . partA}
 
-day02b :: Solution _ _
-day02b = Solution {sParse = Right, sShow = show, sSolve = Right}
+partB :: [Game] -> Int
+partB =
+  sum . map (product . foldr (liftA2 max) (V3 0 0 0) . snd)
+
+day02b :: Solution [Game] Int
+day02b = Solution {sParse = parseInput, sShow = show, sSolve = Right . partB}
