da5b8cce83444ea200b28261e08578bda5528dc3
Matthew Green <mgreen89@gmail.com>
2023-12-03 14:52:07 +0000

    Tidy up day 3

--- src/AoC/Challenge/Day03.hs
+++ src/AoC/Challenge/Day03.hs
@@ -7,26 +7,24 @@ where
 import AoC.Common.Point (allNeighbs)
 import AoC.Solution
 import Data.Char (digitToInt, isDigit)
-import Data.IntSet (IntSet)
+import Data.Foldable (foldl')
 import qualified Data.IntSet as S
+import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Data.Maybe (mapMaybe)
 import Linear (V2 (..))
 
-type PartNo = (Int, V2 Int, Int)
-
-type Symbol = (Char, V2 Int)
-
-type Input = ([PartNo], [Symbol])
+type Input = (Map (V2 Int) Int, Map (V2 Int) Char)
 
 data Chunk
   = Part Int Int
   | Symbol Char
   | Blank
+  deriving (Show)
 
 chunk :: String -> [[Chunk]]
 chunk =
-  fmap (foldl go []) . lines
+  fmap (reverse . foldl' go []) . lines
   where
     go :: [Chunk] -> Char -> [Chunk]
     go a c
@@ -38,49 +36,46 @@ chunk =
 
 parse :: String -> Input
 parse =
-  foldr go ([], []) . zip [0 ..] . chunk
+  foldl' parseLine (M.empty, M.empty) . zip [0 ..] . chunk
   where
-    go :: (Int, [Chunk]) -> Input -> Input
-    go (y, cs) (ps, ss) =
-      snd . foldr go' (0, (ps, ss)) $ cs
-      where
-        go' :: Chunk -> (Int, Input) -> (Int, Input)
-        go' ch (x, (ps', cs')) = case ch of
-          Part i l -> (x + l, ((i, V2 x y, l) : ps', cs'))
-          Symbol c -> (x + 1, (ps', (c, V2 x y) : cs'))
-          Blank -> (x + 1, (ps', cs'))
+    parseLine :: Input -> (Int, [Chunk]) -> Input
+    parseLine (ps, ss) (y, cs) =
+      snd . foldl' (parseChunk y) (0, (ps, ss)) $ cs
+
+    parseChunk :: Int -> (Int, Input) -> Chunk -> (Int, Input)
+    parseChunk y (x, (ps, cs)) ch = case ch of
+      Part i l ->
+        ( x + l,
+          ( M.union
+              ps
+              (M.fromList [(V2 (x + dx) y, i) | dx <- [0 .. (l - 1)]]),
+            cs
+          )
+        )
+      Symbol c -> (x + 1, (ps, M.insert (V2 x y) c cs))
+      Blank -> (x + 1, (ps, cs))
 
 partA :: Input -> Int
-partA (ps, ss) =
-  sum . fmap fst . M.elems . M.filterWithKey nextToSymbol $ partNos
-  where
-    partNos :: M.Map (V2 Int) (Int, Int)
-    partNos = M.fromList . fmap (\(i, s, l) -> (s, (i, l))) $ ps
-
-    symbols :: M.Map (V2 Int) Char
-    symbols = M.fromList . fmap (\(c, p) -> (p, c)) $ ss
-
-    nextToSymbol :: V2 Int -> (Int, Int) -> Bool
-    nextToSymbol (V2 x y) (_, l) =
-      any (any (`M.member` symbols) . allNeighbs) $
-        [V2 (x + dx) y | dx <- [0 .. (l - 1)]]
+partA (parts, syms) =
+  sum
+    . S.toList
+    . S.unions
+    . fmap (S.fromList . mapMaybe (`M.lookup` parts) . allNeighbs)
+    . M.keys
+    $ syms
 
 day03a :: Solution Input Int
 day03a = Solution {sParse = Right . parse, sShow = show, sSolve = Right . partA}
 
 partB :: Input -> Int
-partB (ps, ss) =
-  sum . fmap (S.foldr (*) 1) . M.elems . M.filter ((== 2) . S.size) . M.mapWithKey getPartNos . M.filter (== '*') $ symbols
-  where
-    partNos :: M.Map (V2 Int) Int
-    partNos = M.unions . fmap (\(i, V2 x y, l) -> M.fromList [(V2 (x + dx) y, i) | dx <- [0 .. (l - 1)]]) $ ps
-
-    symbols :: M.Map (V2 Int) Char
-    symbols = M.fromList . fmap (\(c, p) -> (p, c)) $ ss
-
-    getPartNos :: V2 Int -> Char -> IntSet
-    getPartNos p '*' = S.fromList . mapMaybe (`M.lookup` partNos) . allNeighbs $ p
-    getPartNos _ _ = S.empty
+partB (parts, syms) =
+  sum
+    . fmap (product . S.toList)
+    . filter ((== 2) . S.size)
+    . fmap (S.fromList . mapMaybe (`M.lookup` parts) . allNeighbs)
+    . M.keys
+    . M.filter (== '*')
+    $ syms
 
 day03b :: Solution Input Int
 day03b = Solution {sParse = Right . parse, sShow = show, sSolve = Right . partB}
