2087e8571942afb82e1c767c06f6ec5e39c484ff
Matthew Green <mgreen89@gmail.com>
2023-12-04 10:58:59 +0000

    Day 4b

--- src/AoC/Challenge/Day04.hs
+++ src/AoC/Challenge/Day04.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day04
   ( day04a,
+    day04b,
   )
 where
 
--- , day04b
-
 import AoC.Solution
 import Data.Bifunctor (first)
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as S
 import Data.Void (Void)
@@ -19,11 +15,13 @@ import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
 import qualified Text.Megaparsec.Char.Lexer as MPL
 
-type Card = (Int, IntSet, IntSet)
+type Card = (Int, (IntSet, IntSet))
 
-parser :: MP.Parsec Void String [Card]
+type Cards = IntMap (IntSet, IntSet)
+
+parser :: MP.Parsec Void String Cards
 parser = do
-  MP.many cardParser
+  M.fromList <$> MP.many cardParser
   where
     cardParser :: MP.Parsec Void String Card
     cardParser = do
@@ -33,13 +31,13 @@ parser = do
       MP.string "|"
       MP.space
       have <- MP.many (MPL.decimal <* MP.space)
-      pure (cardNo, S.fromList winners, S.fromList have)
+      pure (cardNo, (S.fromList winners, S.fromList have))
 
-parse :: String -> Either String [Card]
+parse :: String -> Either String Cards
 parse =
   first MP.errorBundlePretty . MP.parse parser "day04"
 
-day04a :: Solution [Card] Int
+day04a :: Solution Cards Int
 day04a =
   Solution
     { sParse = parse,
@@ -50,9 +48,29 @@ day04a =
           . fmap
             ( (\x -> if x == 0 then 0 else 2 ^ (x - 1))
                 . S.size
-                . \(_, w, h) -> S.intersection w h
+                . uncurry S.intersection
             )
+          . M.elems
     }
 
-day04b :: Solution _ _
-day04b = Solution {sParse = Right, sShow = show, sSolve = Right}
+partB :: Cards -> Int
+partB cards =
+  sum . M.elems . M.foldlWithKey' go initial $ cards
+  where
+    initial :: IntMap Int
+    initial = M.fromList [(i, 1) | i <- M.keys cards]
+
+    go :: IntMap Int -> Int -> (IntSet, IntSet) -> IntMap Int
+    go counts card (winners, haves) =
+      let winnerCount = S.size $ S.intersection winners haves
+          cardCount = counts M.! card
+          addCards = M.fromList [(c, cardCount) | c <- [card + 1 .. card + winnerCount]]
+       in M.unionWith (+) counts addCards
+
+day04b :: Solution Cards Int
+day04b =
+  Solution
+    { sParse = parse,
+      sShow = show,
+      sSolve = Right . partB
+    }
