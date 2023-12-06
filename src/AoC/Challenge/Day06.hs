32a302633f3cd78e167213dd8ba42e067b2e13ce
Matthew Green <mgreen89@gmail.com>
2023-12-06 09:52:52 +0000

    Day 6a

--- src/AoC/Challenge/Day06.hs
+++ src/AoC/Challenge/Day06.hs
@@ -3,17 +3,37 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-module AoC.Challenge.Day06 (
+module AoC.Challenge.Day06
+  ( day06a,
   )
 where
 
--- day06a
 -- , day06b
 
+import AoC.Common (listTup2)
 import AoC.Solution
+import AoC.Util (maybeToEither)
+import Text.Read (readMaybe)
 
-day06a :: Solution _ _
-day06a = Solution{sParse = Right, sShow = show, sSolve = Right}
+parse :: String -> Maybe [(Int, Int)]
+parse =
+  fmap (uncurry zip)
+    . (>>= listTup2)
+    . traverse (traverse readMaybe . drop 1 . words)
+    . take 2
+    . lines
+
+getWinners :: Int -> Int -> Int
+getWinners t d =
+  sum [if (t - x) * x > d then 1 else 0 | x <- [1 .. (t - 1)]]
+
+day06a :: Solution [(Int, Int)] Int
+day06a =
+  Solution
+    { sParse = maybeToEither "parse error" . parse,
+      sShow = show,
+      sSolve = Right . product . fmap (uncurry getWinners)
+    }
 
 day06b :: Solution _ _
-day06b = Solution{sParse = Right, sShow = show, sSolve = Right}
+day06b = Solution {sParse = Right, sShow = show, sSolve = Right}
