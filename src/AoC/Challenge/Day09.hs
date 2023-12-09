90a6c5bc3d6f77d663ae6183693b94d104c5c072
Matthew Green <mgreen89@gmail.com>
2023-12-09 10:28:07 +0000

    Day 9a

--- src/AoC/Challenge/Day09.hs
+++ src/AoC/Challenge/Day09.hs
@@ -3,17 +3,36 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-module AoC.Challenge.Day09 (
+module AoC.Challenge.Day09
+  ( day09a,
   )
 where
 
--- day09a
 -- , day09b
 
 import AoC.Solution
+import Data.Foldable (foldl')
+import Text.Read (readEither)
 
-day09a :: Solution _ _
-day09a = Solution{sParse = Right, sShow = show, sSolve = Right}
+parse :: String -> Either String [[Int]]
+parse = traverse (traverse readEither . words) . lines
+
+stepBack :: [Int] -> Int
+stepBack =
+  sum . go []
+  where
+    go :: [Int] -> [Int] -> [Int]
+    go a (x : xs) =
+      go (x : a) (zipWith (-) (x : xs) xs)
+    go a [] = a
+
+day09a :: Solution [[Int]] Int
+day09a =
+  Solution
+    { sParse = parse,
+      sShow = show,
+      sSolve = Right . sum . fmap (stepBack . reverse)
+    }
 
 day09b :: Solution _ _
-day09b = Solution{sParse = Right, sShow = show, sSolve = Right}
+day09b = Solution {sParse = Right, sShow = show, sSolve = Right}
