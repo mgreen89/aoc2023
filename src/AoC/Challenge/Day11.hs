89beb641daa819c714958de7cb3cd3d571104385
Matthew Green <mgreen89@gmail.com>
2023-12-11 12:39:32 +0000

    Tidy up day 11

--- src/AoC/Challenge/Day11.hs
+++ src/AoC/Challenge/Day11.hs
@@ -1,14 +1,8 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day11
   ( day11a,
+    day11b,
   )
 where
-
--- , day11b
 
 import AoC.Common.Point (manhattan)
 import AoC.Solution
@@ -32,29 +26,26 @@ parse =
       [0 ..]
     . lines
 
-expand :: Set Point -> Set Point
-expand ps =
+expand :: Int -> Set Point -> Set Point
+expand n ps =
   let (V2 xMax yMax) = S.foldl' (liftA2 max) (V2 0 0) ps
       pList = S.toList ps
-      blankXs = mapMaybe (\i -> i <$ guard (all (\(V2 x _) -> x /= i) pList)) [0 .. xMax]
-      blankYs = mapMaybe (\i -> i <$ guard (all (\(V2 _ y) -> y /= i) pList)) [0 .. yMax]
+      blankXs = filter (\i -> all (\(V2 x _) -> x /= i) pList) [0 .. xMax]
+      blankYs = filter (\i -> all (\(V2 _ y) -> y /= i) pList) [0 .. yMax]
 
       go :: Point -> Point
-      go (V2 x y) = V2 (x + length (takeWhile (< x) blankXs)) (y + length (takeWhile (< y) blankYs))
+      go (V2 x y) =
+        V2
+          (x + (n - 1) * length (takeWhile (< x) blankXs))
+          (y + (n - 1) * length (takeWhile (< y) blankYs))
    in S.map go ps
 
-solveA :: Set Point -> Int
-solveA ps =
-  let allPairs :: [a] -> [(a, a)]
-      allPairs = (concatMap pairsFromFirst . tails)
-        where
-          pairsFromFirst :: [a] -> [(a, a)]
-          pairsFromFirst (a : b : xs) = (a, b) : pairsFromFirst (a : xs)
-          pairsFromFirst _ = []
-   in sum [manhattan p q | (p, q) <- allPairs (S.toList (expand ps))]
+solve :: Int -> Set Point -> Int
+solve n ps =
+  sum [ manhattan p q | p : qs <- tails (S.toList (expand n ps)), q <- qs ]
 
 day11a :: Solution (Set Point) Int
-day11a = Solution {sParse = Right . parse, sShow = show, sSolve = Right . solveA}
+day11a = Solution {sParse = Right . parse, sShow = show, sSolve = Right . solve 2}
 
-day11b :: Solution _ _
-day11b = Solution {sParse = Right, sShow = show, sSolve = Right}
+day11b :: Solution (Set Point) Int
+day11b = Solution {sParse = Right . parse, sShow = show, sSolve = Right . solve 1000000}
