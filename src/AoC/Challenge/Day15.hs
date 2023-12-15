0aee4060c7bfcc908cd1bb92f100d91490084f63
Matthew Green <matgreen@cisco.com>
2023-12-15 09:45:04 +0000

    Day 15a

--- src/AoC/Challenge/Day15.hs
+++ src/AoC/Challenge/Day15.hs
@@ -4,16 +4,24 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day15 (
+  day15a,
   )
 where
 
--- day15a
 -- , day15b
 
 import AoC.Solution
+import Data.Char (ord)
+import Data.Foldable (foldl')
+import Data.List.Split (splitOn)
 
-day15a :: Solution _ _
-day15a = Solution{sParse = Right, sShow = show, sSolve = Right}
+hash :: String -> Int
+hash = foldl' go 0
+  where
+  go a = (`mod` 256) . (* 17) . (+ a) . ord
+
+day15a :: Solution [String] Int
+day15a = Solution{sParse = Right . splitOn ",", sShow = show, sSolve = Right . sum . fmap hash }
 
 day15b :: Solution _ _
 day15b = Solution{sParse = Right, sShow = show, sSolve = Right}
