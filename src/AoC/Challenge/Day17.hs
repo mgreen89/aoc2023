f1f62cff032e827d7cb2bfdac703eabd4a8964fb
Matthew Green <mgreen89@gmail.com>
2023-12-18 14:41:21 +0000

    Day 17b progress

--- src/AoC/Challenge/Day17.hs
+++ src/AoC/Challenge/Day17.hs
@@ -1,29 +1,24 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day17
   ( day17a,
+    day17b,
   )
 where
-
--- , day17b
 
 import AoC.Common.Graph (aStarWithPath)
 import AoC.Common.Point (Dir (..), dirPoint, dirRot, manhattan, parse2dMap)
 import AoC.Solution
 import AoC.Util (maybeToEither)
-import Control.Monad (guard)
+import Data.Bifunctor (first)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (mapMaybe, maybeToList)
 import Linear (V2 (..))
 
 type Point = V2 Int
 
-solveA :: Map Point Int -> Either String Int
-solveA m =
+solve :: Int -> Int -> Map Point Int -> Either String Int
+solve minStep maxStep m =
   fmap fst
     . maybeToEither "aStar failed"
     . aStarWithPath (manhattan finish . fst) getNeighbs (start, U)
@@ -31,6 +26,22 @@ solveA m =
   where
     start = V2 0 0
     finish = maximum $ M.keys m
+
+    spanDir :: Point -> Dir -> [(Point, Int)]
+    spanDir p d =
+      NE.drop minStep
+        . NE.reverse
+        . go 1
+        $ ((p, 0) NE.:| [])
+      where
+        go :: Int -> NonEmpty (Point, Int) -> NonEmpty (Point, Int)
+        go i a@((lastP, lastC) NE.:| _)
+          | i > maxStep = a
+          | otherwise =
+              let next = (lastP + dirPoint d)
+               in case M.lookup next m of
+                    Nothing -> a
+                    Just c -> go (i + 1) ((next, lastC + c) NE.<| a)
 
     getNeighbs :: [(Point, Dir)] -> (Point, Dir) -> Map (Point, Dir) Int
     getNeighbs parents (p, d) =
@@ -41,25 +52,13 @@ solveA m =
               then -- Only move R and D.
                 [R, D]
               else -- Change direction, and don't allow backtracking.
-                filter (/= d) . filter (/= dirRot D d) $ [R, D, L, U]
-
-          getNexts :: Dir -> [((Point, Dir), Int)]
-          getNexts d' =
-            tail . reverse . go 1 $ [((p, d), 0)]
-            where
-              dirMove = dirPoint d'
-              go :: Int -> [((Point, Dir), Int)] -> [((Point, Dir), Int)]
-              go n as@(((p', _), c) : _)
-                | n > 3 = as
-                | otherwise =
-                    case M.lookup (p' + dirMove) m of
-                      Nothing -> as
-                      Just inc -> go (n + 1) (((p' + dirMove, d'), c + inc) : as)
-              go _ _ = error "failed"
-       in M.fromList . concatMap getNexts $ possDirections
+                [dirRot L d, dirRot R d]
+       in M.fromList
+            . concatMap (\d' -> first (,d') <$> spanDir p d')
+            $ possDirections
 
 day17a :: Solution (Map Point Int) Int
-day17a = Solution {sParse = parse2dMap, sShow = show, sSolve = solveA}
+day17a = Solution {sParse = parse2dMap, sShow = show, sSolve = solve 1 3}
 
-day17b :: Solution _ _
-day17b = Solution {sParse = Right, sShow = show, sSolve = Right}
+day17b :: Solution (Map Point Int) Int
+day17b = Solution {sParse = parse2dMap, sShow = show, sSolve = solve 4 10}
