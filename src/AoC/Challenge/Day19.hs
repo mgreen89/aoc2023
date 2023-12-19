662736e092c9c61bd20582b14fe041379bf70042
Matthew Green <matgreen@cisco.com>
2023-12-19 16:28:11 +0000

    Day 19b incomplete

--- src/AoC/Challenge/Day19.hs
+++ src/AoC/Challenge/Day19.hs
@@ -1,19 +1,18 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day19 (
   day19a,
+  day19b,
   )
 where
-
--- , day19b
 
 import AoC.Solution
 import Control.DeepSeq (NFData)
 import Data.Bifunctor (first)
 import Data.Char (isLower)
+import Data.Foldable (foldl')
+import Data.Interval (Interval, Boundary(..))
+import qualified Data.Interval as IV
+import Data.IntervalSet (IntervalSet)
+import qualified Data.IntervalSet as IVS
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Void (Void)
@@ -21,6 +20,8 @@ import GHC.Generics (Generic)
 import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
 import qualified Text.Megaparsec.Char.Lexer as MPL
+
+import Debug.Trace
 
 data Part = Part { x :: Int, m :: Int, a :: Int, s :: Int }
   deriving (Eq, Generic, NFData, Show)
@@ -114,5 +115,106 @@ solveA (instrs, ps) =
 day19a :: Solution (Map String Instruction, [Part]) Int
 day19a = Solution{sParse = parse, sShow = show, sSolve = Right . solveA}
 
-day19b :: Solution _ _
-day19b = Solution{sParse = Right, sShow = show, sSolve = Right}
+data RangePart = RangePart { x :: IntervalSet Int, m :: IntervalSet Int, a :: IntervalSet Int, s :: IntervalSet Int }
+  deriving (Eq, Generic, NFData, Show)
+
+-- Can't just do this - duh!
+union :: RangePart -> RangePart -> RangePart
+union a b =
+  RangePart
+    (IVS.union a.x b.x)
+    (IVS.union a.m b.m)
+    (IVS.union a.a b.a)
+    (IVS.union a.s b.s)
+
+data RangeRule = RangeRule { applyRange :: (RangePart -> Maybe RangePart), target :: Workflow }
+  deriving (Generic, NFData)
+
+newtype RangeInstruction = RangeInstruction { rules :: [RangeRule] }
+  deriving (Generic, NFData)
+
+rangeParser :: MP.Parsec Void String (Map String RangeInstruction)
+rangeParser = do
+  ranges <- MP.some $ do
+      name <- MP.takeWhile1P (Just "name") (isLower)
+      MP.char '{'
+      rules <- MP.sepBy rangeRuleParser (MP.char ',')
+      MP.char '}'
+      MP.space
+      pure (name, RangeInstruction rules)
+  pure $ M.fromList ranges
+
+  where
+    rangeRuleParser :: MP.Parsec Void String RangeRule
+    rangeRuleParser = do
+      range <- MP.choice [MP.try rangeConditionParser, (pure (Just))]
+      target <- MP.choice [
+        Rejected <$ MP.char 'R',
+        Accepted <$ MP.char 'A',
+        Named <$> MP.takeWhile1P (Just "workflow") (isLower)
+        ]
+      pure $ RangeRule range target
+
+    rangeConditionParser :: MP.Parsec Void String (RangePart -> Maybe RangePart)
+    rangeConditionParser = do
+      (get, set) <- MP.choice [
+        ((.x), (\rp a -> (rp { x = a }) :: RangePart)) <$ MP.char 'x',
+        ((.m), (\rp a -> (rp { m = a }) :: RangePart)) <$ MP.char 'm',
+        ((.a), (\rp a -> (rp { a = a }) :: RangePart)) <$ MP.char 'a',
+        ((.s), (\rp a -> (rp { s = a }) :: RangePart)) <$ MP.char 's'
+        ]
+      rangeGen <- MP.choice [
+        (\v -> IV.Finite 0 IV.<..< IV.Finite v) <$ MP.char '<',
+        (\v -> IV.Finite v IV.<..<= IV.Finite 4000) <$ MP.char '>'
+        ]
+      val <- MPL.decimal
+      MP.char ':'
+      let rs = IVS.singleton (rangeGen val)
+      pure $ (\rp ->
+        let is = IVS.intersection (get rp) rs
+        in if (IVS.null is) then Nothing else Just (set rp is))
+
+parseB :: String -> Either String (Map String RangeInstruction)
+parseB =
+  first MP.errorBundlePretty . MP.parse rangeParser "day19b"
+
+ivSize :: Interval Int -> Int
+ivSize iv =
+  case (IV.lowerBound' iv, IV.upperBound' iv) of
+    ((IV.Finite l, lb), (IV.Finite u, ub)) ->
+      let lbreal = case lb of
+                     Open -> l + 1
+                     Closed -> l
+          ubreal = case ub of
+                     Open -> u - 1
+                     Closed -> u
+      in ubreal - lbreal + 1
+    _ -> error "Infinite interval!"
+
+ivsSize :: IntervalSet Int -> Int
+ivsSize = sum . fmap ivSize . IVS.toList
+
+poss :: RangePart -> Int
+poss rp = (ivsSize rp.x) * (ivsSize rp.m) * (ivsSize rp.a) * (ivsSize rp.s)
+
+solveB :: Map String RangeInstruction -> Int
+solveB m = 
+  poss . traceShowId $ go (m M.! "in") (RangePart fullRange fullRange fullRange fullRange)
+  where
+    fullRange = IVS.singleton (IV.Finite 1 IV.<=..<= IV.Finite 4000)
+
+    go :: RangeInstruction -> RangePart -> RangePart
+    go ri rp =
+      foldl' (go' rp) (RangePart IVS.empty IVS.empty IVS.empty IVS.empty) ri.rules
+
+    go' :: RangePart -> RangePart -> RangeRule -> RangePart 
+    go' rp a rr =
+        case rr.applyRange rp of
+          Nothing -> (trace "no overlap" a)
+          Just rp' -> case rr.target of
+            Rejected -> (trace ("rejected " ++ show rp') a)
+            Accepted -> (trace ("accepted " ++ (show rp')) union a rp')
+            Named w -> (trace ("going to " ++ w ++ " " ++ (show rp')) union a (go (m M.! w) rp'))
+
+day19b :: Solution (Map String RangeInstruction) Int
+day19b = Solution{sParse = parseB, sShow = show, sSolve = Right . solveB }
