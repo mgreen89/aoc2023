749a7a265d997edc297f9138b1eb4c06521d6895
Matthew Green <mgreen89@gmail.com>
2023-12-31 12:04:25 +0000

    Tidy up day 22a

--- src/AoC/Challenge/Day22.hs
+++ src/AoC/Challenge/Day22.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day22
@@ -14,10 +13,10 @@ import AoC.Solution
 import Control.Lens ((&), (.~), (^.))
 import Data.Bifunctor (first)
 import Data.Foldable (foldl')
-import Data.List (sortOn)
+import Data.List (nub, sortOn)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (fromMaybe)
 import Data.Void (Void)
 import Linear (V2 (..), V3 (..), _z)
 import qualified Text.Megaparsec as MP
@@ -46,47 +45,33 @@ getXYs (V3 x1 y1 _, V3 x2 y2 _)
   | y1 /= y2 = [V2 x1 y | y <- [y1 .. y2]]
   | otherwise = [V2 x1 y1]
 
-getXYZs :: Block -> [V3 Int]
-getXYZs (V3 x1 y1 z1, V3 x2 y2 z2)
-  | x1 /= x2 = [V3 x y1 z1 | x <- [x1 .. x2]]
-  | y1 /= y2 = [V3 x1 y z1 | y <- [y1 .. y2]]
-  | z1 /= z2 = [V3 x1 y1 z | z <- [z1 .. z2]]
-  | otherwise = [V3 x1 y1 z1]
-
-settle :: [Block] -> ([Block], Int)
+-- Settle the blocks, and return a map of settle block to blocks that support it.
+settle :: [Block] -> Map Block [Block]
 settle =
-  snd . foldl' go (M.empty, ([], 0)) . sortOn ((^. _z) . fst)
+  snd . foldl' go (M.empty, M.empty) . sortOn ((^. _z) . fst)
   where
-    go :: (Map (V2 Int) Int, ([Block], Int)) -> Block -> (Map (V2 Int) Int, ([Block], Int))
-    go (m, (bs, a)) b@(b1@(V3 _ _ z1), b2@(V3 _ _ z2)) =
+    go ::
+      (Map (V2 Int) (Int, Block), Map Block [Block]) ->
+      Block ->
+      (Map (V2 Int) (Int, Block), Map Block [Block])
+    go (highMap, supportMap) b@(b1@(V3 _ _ z1), b2@(V3 _ _ z2)) =
       let xys = getXYs b
-          z' = (+ 1) . maximum . fmap (fromMaybe 0 . (m M.!?)) $ xys
-          new = M.fromList . fmap (,z' + z2 - z1) $ xys
+          possSupports = nub . fmap (fromMaybe (0, (0, 0)) . (highMap M.!?)) $ xys
+          z' = (+ 1) . maximum . fmap fst $ possSupports
+          supports = nub . fmap snd $ filter ((== (z' - 1)) . fst) possSupports
           b' = (b1 & _z .~ z', b2 & _z .~ (z' + z2 - z1))
-       in (M.union new m, (b' : bs, if b == b' then a else a + 1))
+          newHeights = M.fromList . fmap (,(z' + z2 - z1, b')) $ xys
+       in (M.union newHeights highMap, M.insert b' supports supportMap)
 
 solveA :: [Block] -> Int
 solveA bs =
-  length . filter id . fmap couldDisintegrate $ settled
+  length . filter id . fmap couldDisintegrate . M.keys $ settled
   where
-    settled :: [Block]
-    settled = sortOn ((^. _z) . fst) . fst . settle $ bs
-
-    xyzToBlock :: Map (V3 Int) Block
-    xyzToBlock = M.fromList . concatMap (\b -> [(xyz, b) | xyz <- getXYZs b]) $ settled
-
-    getSupports :: Block -> [Block]
-    getSupports b =
-      let getSupport :: V3 Int -> Maybe Block
-          getSupport t@(V3 _ _ z)
-            | z == 1 = Just (V3 0 0 0, V3 0 0 0) -- Dummy "ground" block
-            | otherwise =
-                (\x -> if x == b then Nothing else Just x)
-                  =<< M.lookup (t - V3 0 0 1) xyzToBlock
-       in mapMaybe getSupport . getXYZs $ b
+    settled :: Map Block [Block]
+    settled = settle bs
 
     couldDisintegrate :: Block -> Bool
-    couldDisintegrate b = all (any (/= b) . getSupports) settled
+    couldDisintegrate b = not . or . M.elems . M.map (== [b]) $ settled
 
 day22a :: Solution [Block] Int
 day22a = Solution {sParse = parse, sShow = show, sSolve = Right . solveA}
